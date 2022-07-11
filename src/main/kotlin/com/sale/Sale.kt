20ac395a11fd7ac3a37a31a8df3ced57154825cc
Elaheh Shoghi <e.shoghi@rahkar.co>
2022-07-11 15:35:48 +0430

    Refactor

--- src/main/kotlin/com/sale/Sale.kt
+++ src/main/kotlin/com/sale/Sale.kt
@@ -3,7 +3,8 @@ package com.sale
 class Sale(private val priceService: PriceService) {
 
     fun total(vararg barcode: String): String {
-        val errorMessage = barcode.map { priceService.getPriceByBarcode(it) }.firstOrNull { !it.contains("$") }
-        return errorMessage ?: "$${barcode.sumOf { priceService.getPriceByBarcode(it).replace("$", "").toDouble() }}"
+        val barcodePrice = barcode.map { priceService.getPriceByBarcode(it) }
+        val errorMessage = barcodePrice.firstOrNull { !it.contains("$") }
+        return errorMessage ?: "$${barcodePrice.sumOf { it.replace("$", "").toDouble() }}"
     }
 }
--- src/test/kotlin/com/sale/SaleTest.kt
+++ src/test/kotlin/com/sale/SaleTest.kt
@@ -1,18 +1,25 @@
 package com.sale
 
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import kotlin.test.assertEquals
 
 class SaleTest {
     private val sale = Sale(PriceService())
 
-    @Test
-    fun `should display the sum of the scanned product prices`() {
-        assertEquals("$19.75", sale.total("12345", "23456"))
+    companion object {
+        @JvmStatic
+        fun barcodesWithTotalPriceArguments() = listOf(
+            Arguments.of(arrayOf("12345", "23456"), "$19.75"),
+            Arguments.of(arrayOf("12345", "23456", "4f7s"), "Error: barcode not found"),
+            Arguments.of(arrayOf("12345", "", "4f7s"), "Error: empty barcode"),
+        )
     }
 
-    @Test
-    fun `should display appropriate message for invalid barcode`() {
-        assertEquals("Error: barcode not found", sale.total("12345", "23456", "4f7s"))
+    @ParameterizedTest
+    @MethodSource("barcodesWithTotalPriceArguments")
+    fun `should display sum of product prices correctly`(vararg barcodes: String, expectedResult: String) {
+        assertEquals(expectedResult, sale.total(*barcodes))
     }
 }
