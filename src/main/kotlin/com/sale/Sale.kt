53572ec8cc88ad59578e6a0f3329f786c0eb083f
Elaheh Shoghi <e.shoghi@rahkar.co>
2022-07-11 13:21:32 +0430

    Pass the test.

--- src/main/kotlin/com/sale/Sale.kt
+++ src/main/kotlin/com/sale/Sale.kt
@@ -2,7 +2,7 @@ package com.sale
 
 class Sale {
     fun getPriceByBarcode(barcode: String): String {
-        return if (barcode == "12345") "$7.25" else if (barcode == "23456") "$12.50" else "Error: barcode not found"
+        return if (barcode == "") "Error: empty barcode" else if (barcode == "12345") "$7.25" else if (barcode == "23456") "$12.50" else "Error: barcode not found"
     }
 
 }
