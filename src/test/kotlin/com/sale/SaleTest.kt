d9500a9eb89bc8f5e3d4330fabedb8e4b2304821
Elaheh Shoghi <e.shoghi@rahkar.co>
2022-07-11 12:39:29 +0430

    Create the test case to check that barcode '23456' cost '$12.50'.

--- src/test/kotlin/com/sale/SaleTest.kt
+++ src/test/kotlin/com/sale/SaleTest.kt
@@ -9,4 +9,9 @@ class SaleTest {
     fun `should display price $7,25 for barcode 12345`() {
         assertEquals("$7.25", Sale().getPriceByBarcode("12345"))
     }
+
+    @Test
+    fun `should display price $12,50 for barcode 23456`() {
+        assertEquals("$12.50", Sale().getPriceByBarcode("23456"))
+    }
 }
