8bf6f4c0cf795fdc41be8448c7ef3e1403389987
Elaheh Shoghi <e.shoghi@rahkar.co>
2022-07-11 15:17:54 +0430

    Create the test case to check that it displays appropriate messages for invalid barcodes in total function.

--- src/test/kotlin/com/sale/SaleTest.kt
+++ src/test/kotlin/com/sale/SaleTest.kt
@@ -10,4 +10,9 @@ class SaleTest {
     fun `should display the sum of the scanned product prices`() {
         assertEquals("$19.75", sale.total("12345", "23456"))
     }
+
+    @Test
+    fun `should display appropriate message for invalid barcode`() {
+        assertEquals("Error: barcode not found", sale.total("12345", "23456", "4f7s"))
+    }
 }
