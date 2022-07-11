c4c87afbeeaa9268aff277ea08a1689cc6b1604e
Elaheh Shoghi <e.shoghi@rahkar.co>
2022-07-11 15:24:27 +0430

    Pass the test.

--- src/main/kotlin/com/sale/Sale.kt
+++ src/main/kotlin/com/sale/Sale.kt
@@ -2,6 +2,8 @@ package com.sale
 
 class Sale(private val priceService: PriceService) {
 
-    fun total(vararg barcode: String) =
-        "$${barcode.sumOf { priceService.getPriceByBarcode(it).replace("$", "").toDouble() }}"
+    fun total(vararg barcode: String): String {
+        val errorMessage = barcode.map { priceService.getPriceByBarcode(it) }.firstOrNull { !it.contains("$") }
+        return errorMessage ?: "$${barcode.sumOf { priceService.getPriceByBarcode(it).replace("$", "").toDouble() }}"
+    }
 }
