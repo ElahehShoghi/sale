a88da13d1c840a2634953f46dae1f39b6dda8d69
Elaheh Shoghi <e.shoghi@rahkar.co>
2022-07-11 13:26:03 +0430

    Refactor

--- src/main/kotlin/com/sale/Sale.kt
+++ src/main/kotlin/com/sale/Sale.kt
@@ -1,8 +1,11 @@
 package com.sale
 
 class Sale {
-    fun getPriceByBarcode(barcode: String): String {
-        return if (barcode == "") "Error: empty barcode" else if (barcode == "12345") "$7.25" else if (barcode == "23456") "$12.50" else "Error: barcode not found"
+    fun getPriceByBarcode(barcode: String): String = when (barcode) {
+        "" -> "Error: empty barcode"
+        "12345" -> "$7.25"
+        "23456" -> "$12.50"
+        else -> "Error: barcode not found"
     }
 
 }
