12a7d392169054941ff4db34f9c814151e39d11a
Elaheh Shoghi <e.shoghi@rahkar.co>
2022-07-12 19:23:53 +0430

    Implement interactive app with its test.

--- src/main/kotlin/com/sale/Sale.kt
+++ src/main/kotlin/com/sale/Sale.kt
@@ -1,8 +1,32 @@
 package com.sale
 
-class Sale {
-    fun getPriceByBarcode(barcode: String): String {
-        return "$7.25"
+class Sale(private val inputReader: BarcodeReader, private val outputWriter: OutputWriter) {
+
+
+
+    fun startApp() {
+        val inputBarcode = inputReader()
+        if (inputBarcode == "23456")
+            outputWriter("$12.5")
+        else if (inputBarcode == "12345")
+            outputWriter("$7.25")
     }
 
+    fun interface BarcodeReader {
+        operator fun invoke(): String
+    }
+
+    fun interface OutputWriter {
+        operator fun invoke(outputString: String)
+    }
+}
+
+fun main() {
+    val sale = Sale({
+        readln()
+    },
+        {
+            println(it)
+        })
+    sale.startApp()
 }
--- src/test/kotlin/com/sale/SaleTest.kt
+++ src/test/kotlin/com/sale/SaleTest.kt
@@ -1,12 +1,47 @@
 package com.sale
 
-import org.junit.Test
-import kotlin.test.assertEquals
+import io.mockk.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 class SaleTest {
 
-    @Test
-    fun `should display price $7,25 for barcode 12345`() {
-        assertEquals("$7.25", Sale().getPriceByBarcode("12345"))
+    companion object{
+
+        @JvmStatic
+        fun saleItemsBarcodeAndPrice() = listOf(
+            Arguments.of("12345", "$7.25"),
+            Arguments.of("23456", "$12.5"),
+        )
     }
+
+    @MethodSource("saleItemsBarcodeAndPrice")
+    @ParameterizedTest
+    fun `should display correct price for barcode`(barcode: String, price: String){
+        val input: Sale.BarcodeReader = mockk()
+        every { input.invoke() } returns barcode
+        val output: Sale.OutputWriter = mockk()
+        every { output.invoke(any()) } just runs
+        val sale = Sale(input, output)
+        sale.startApp()
+        verify {
+            output.invoke(price)
+        }
+        verify(exactly = 1) {
+            output.invoke(any())
+        }
+    }
+
+    @Test
+    fun `should output nothing for invalid barcode`(){
+        val input: Sale.BarcodeReader = mockk()
+        every { input.invoke() } returns "invalid barcode"
+        val output: Sale.OutputWriter = mockk()
+        val sale = Sale(input, output)
+        sale.startApp()
+        verify { input.invoke() }
+    }
+
 }
